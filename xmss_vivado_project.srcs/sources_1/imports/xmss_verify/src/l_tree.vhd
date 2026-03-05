2720178a0c66a1f2142ea9c5b8d043c06e4e8e23
javierrrsanz <javierrr_sanz@hotmail.com>
2026-03-05 17:03:50 +0100

    más limpieza

--- xmss_vivado_project.srcs/sources_1/imports/xmss_verify/src/l_tree.vhd
+++ xmss_vivado_project.srcs/sources_1/imports/xmss_verify/src/l_tree.vhd
@@ -1,18 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: Ruhr-University Bochum / Chair for Security Engineering
--- Engineer: Jan Philipp Thoma
--- 
--- Create Date: 13.08.2020
--- Project Name: Full XMSS Hardware Accelerator
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.xmss_main_typedef.ALL;
 use work.params.ALL;
-USE ieee.numeric_std.ALL; 
-
+use ieee.numeric_std.ALL; 
 
 entity l_tree is
     port (
@@ -25,116 +15,108 @@ end l_tree;
 architecture Behavioral of l_tree is
     alias m_in : xmss_l_tree_input_type_small is d.module_input;
     alias m_out : xmss_l_tree_output_type_small is q.module_output;   
-    type state_type is (S_IDLE, S_LOOP, S_INNER_LOOP, S_THASH, S_BRAM_WAIT_1, S_BRAM_WAIT_2 , S_SWITCH_READ, S_SWITCH_WRITE, S_BRAM_WAIT);
+    
+    type state_type is (S_IDLE, S_LOOP, S_INNER_LOOP, S_THASH, S_SWITCH_READ, S_SWITCH_WRITE, S_BRAM_WAIT, S_BRAM_SYNC);
+
     type reg_type is record
-        state : state_type;       
-        l : unsigned(wots_len_log - 1 downto 0);--integer range 0 to wots_len;
-        height : integer range 0 to wots_len_log;
-        parent_node : unsigned(wots_len_log - 1 downto 0);-- integer;
-        ctr : integer range 0 to wots_len; 
+        state        : state_type;
+        l            : unsigned(wots_len_log - 1 downto 0);
+        height       : integer range 0 to wots_len_log;
+        parent_node  : unsigned(wots_len_log - 1 downto 0);
+        ctr          : integer range 0 to wots_len; 
     end record;
+
     signal bram_select : unsigned(1 downto 0);
-    signal DEBUG_LTREE_DONE : std_logic;
-    signal DEBUG_LTREE_EN : std_logic;
     signal r, r_in : reg_type;
 begin
-    DEBUG_LTREE_EN <= m_in.enable;
-    --------------------------
-    -- Static output wiring --
-    --------------------------
-    q.bram.a.en <= '1';
-    q.bram.a.wen <= '0';
-    q.bram.a.din <= (others => '-');
+
+    -- Asignaciones estaticas de puertos (Puerto A para lectura base)
+    q.bram.a.en   <= '1';
+    q.bram.a.wen  <= '0';
+    q.bram.a.din  <= (others => '-');
     q.bram.a.addr <= std_logic_vector(to_unsigned(BRAM_WOTS_KEY + 2 * r.ctr, BRAM_ADDR_SIZE));
     
-    q.bram.b.en <= '1';
+    -- Puerto B para escritura de nodos intermedios y lectura del segundo operando
+    q.bram.b.en  <= '1';
     q.bram.b.din <= d.thash.o when bram_select = "10" else d.bram.a.dout;
     
-    q.thash.input_1 <= d.bram.a.dout;
-    q.thash.input_2 <= d.bram.b.dout;
+    q.thash.input_1   <= d.bram.a.dout;
+    q.thash.input_2   <= d.bram.b.dout;
     q.thash.address_3 <= x"00000001";
     q.thash.address_4 <= m_in.address_4;
     q.thash.address_5 <= std_logic_vector(to_unsigned(r.height, 32));
     q.thash.address_6 <= std_logic_vector(to_unsigned(r.ctr, 32));
 
     m_out.leaf_node <= d.thash.o;
-    
-    combinational : process (r, d)
+
+    combinational : process (r, d, bram_select)
 	   variable v : reg_type;
 	begin
-	    v := r; 
-        DEBUG_LTREE_DONE <= '0';
-        -- Default assignments
+	    v := r;
         q.thash.enable <= '0';
-        q.bram.b.wen <= '0';
-        m_out.done <= '0';
-        bram_select <= (others => '0');
+        q.bram.b.wen   <= '0';
+        m_out.done     <= '0';
+        bram_select    <= "00"; -- Por defecto lee 2*ctr + 1
         
      	case r.state is
      	      when S_IDLE =>
      	          if m_in.enable = '1' then
-     	              -- Initialize variables
      	              v.l := to_unsigned(WOTS_LEN, wots_len_log);
      	              v.height := 0;
      	              v.ctr := 0;
      	              v.state := S_BRAM_WAIT;
      	          end if;
+
      	      when S_LOOP => 
-     	          -- l couts the remaning n-bit hashes to be compressed
      	          if r.l > 1 then
      	              v.parent_node := shift_right(r.l, 1);
      	              v.state := S_INNER_LOOP;
      	          else 
      	              v.state := S_IDLE;
      	              m_out.done <= '1';
-     	              DEBUG_LTREE_DONE <= '1';
      	          end if;
+
      	      when S_INNER_LOOP =>
      	          if r.ctr < r.parent_node then
-     	              -- Use thash to compress the next two n-bit values
-     	              -- Inputs from BRAM
      	              q.thash.enable <= '1';
      	              v.state := S_THASH;
      	          else
-     	              -- If the remaining number of nodes is even, start next loop iteration
-     	              -- otherwise copy the remaining "odd" value on top of the reduced stack
+                      -- Gestion de nodos impares y subida de nivel
      	              v.height := r.height + 1;
      	              if r.l mod 2 = 1 then
-     	                  bram_select <= "01"; -- read the reminaing 2n+1-th value
      	                  v.l := shift_right(r.l, 1) + 1;
      	                  v.state := S_SWITCH_READ; 
      	              else 
-     	                  v.l := shift_right(r.l, 1); 
-     	                  v.state := S_BRAM_WAIT;
+     	                  v.l := shift_right(r.l, 1);
      	                  v.ctr := 0;
+                          v.state := S_BRAM_WAIT;
      	              end if;
      	          end if;
+
      	      when S_SWITCH_READ =>
-     	          -- Read takes two cycles --> wait
-     	          bram_select <= "01";
+     	          bram_select <= "01"; -- Direccion simplificada
      	          v.state := S_SWITCH_WRITE;
+
      	      when S_SWITCH_WRITE => 
-     	          -- write the read value to the new position and reset the counter
      	          bram_select <= "01";
      	          q.bram.b.wen <= '1';
      	          v.ctr := 0;
      	          v.state := S_BRAM_WAIT;
+
      	      when S_BRAM_WAIT => 
-     	          -- make sure, BRAM reads are finished in time
      	          v.state := S_LOOP;
+
      	      when S_THASH =>
-     	          -- wait until thahs algorithm is done and increase counter
      	          if d.thash.done = '1' then
      	              bram_select <= "10";
-     	              q.bram.b.wen <= '1'; -- write new value...
-     	              v.ctr := r.ctr +1;
-                      v.state := S_BRAM_WAIT_1;
+     	              q.bram.b.wen <= '1';
+     	              v.ctr := r.ctr + 1;
+     	              v.state := S_BRAM_SYNC; -- Un solo ciclo de sincronización es suficiente
      	          end if;
-     	      when S_BRAM_WAIT_1 =>
-     	          -- Make sure BRAM reads are ready when entering inner Loop...
-     	          v.state := S_BRAM_WAIT_2;
-     	      when  S_BRAM_WAIT_2 =>
+
+     	      when S_BRAM_SYNC =>
      	          v.state := S_INNER_LOOP;
+
      	end case;
      	r_in <= v;
     end process;     
@@ -142,20 +124,15 @@ begin
     bram_mux : process(bram_select, r.ctr)
     begin
         case bram_select is
-            
-     	    when "01" =>
+            -- Caso "01" y "10" unificados: escritura/copia de nodos reducidos
+     	    when "01" | "10" =>
      	            q.bram.b.addr <= std_logic_vector(to_unsigned(BRAM_WOTS_KEY + r.ctr, BRAM_ADDR_SIZE));
-     	    when "10" =>
-                    q.bram.b.addr <= std_logic_vector(to_unsigned(BRAM_WOTS_KEY + r.ctr, BRAM_ADDR_SIZE));
-     	    when "00" =>
-     	            q.bram.b.addr <= std_logic_vector(to_unsigned(BRAM_WOTS_KEY + 2 * r.ctr + 1, BRAM_ADDR_SIZE));
+            -- Caso "00": lectura del segundo operando (2*ctr + 1)
      	    when others =>
-     	            q.bram.b.addr <= (others => '-');
+     	            q.bram.b.addr <= std_logic_vector(to_unsigned(BRAM_WOTS_KEY + 2 * r.ctr + 1, BRAM_ADDR_SIZE));
         end case;
     end process;
    
-   
-    
     sequential : process(clk)
 	begin
 	   if rising_edge(clk) then
@@ -167,4 +144,3 @@ begin
        end if;
     end process;
 end Behavioral;
-
--- xmss_vivado_project.srcs/sources_1/imports/xmss_verify/src/params.vhd
+++ xmss_vivado_project.srcs/sources_1/imports/xmss_verify/src/params.vhd
@@ -1,8 +1,3 @@
-----------------------------------------------------------------------------------
--- This file specifies global parameters
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -10,9 +5,8 @@ use IEEE.NUMERIC_STD.ALL;
 package params is
    
    constant HASH_CORES : Integer := 1; 
-   constant HASH_CHAINS: Integer := 1; 
-
-   constant HASH_FUNCTION : STRING := "SHA"; -- Possible Values are SHA (for SHA2-256) and SHAKE (for SHAKE-128) and SHA_FAST (for optimized SHA2)
+   constant HASH_CHAINS: Integer := 1;
+   constant HASH_FUNCTION : STRING := "SHA"; 
 
    constant n               : Integer := 32;
    constant wots_w          : Integer := 16;
@@ -21,42 +15,43 @@ package params is
    constant wots_len2       : Integer := 3;
    constant wots_len        : Integer := wots_len1 + wots_len2;
    constant wots_len_log    : Integer := 7;
-   constant tree_height     : Integer := 10;--10;
-
-   -- Lenght of the counter used for hash bus addressing
+   constant tree_height     : Integer := 10;
    constant ID_CTR_LEN : Integer := wots_len_log;
 
-   -- BRAM Sections
-   constant BRAM_PK : integer := 0;
-   constant BRAM_TREEHASH_INTER : integer := 1;
-   constant BRAM_TREEHASH_STACK : integer := BRAM_TREEHASH_INTER + (2**tree_height); -- 5
-   constant BRAM_WOTS_KEY : integer := BRAM_TREEHASH_STACK + tree_height + 1; -- 8
-   constant BRAM_XMSS_SIG_WOTS: integer := BRAM_WOTS_KEY + WOTS_LEN; -- 66
-   constant BRAM_XMSS_SIG_AUTH : integer := BRAM_XMSS_SIG_WOTS + WOTS_LEN; -- 142
-   constant BRAM_XMSS_SIG : integer := BRAM_XMSS_SIG_AUTH + tree_height; --144
-   constant BRAM_MESSAGE : integer := BRAM_XMSS_SIG + 4; -- 148   
+   -- BRAM Sections (Optimizado para Verify-Only)
+   -- Se eliminan las secciones de TreeHash intermedio y Stack usadas solo en firma
+   constant BRAM_PK             : integer := 0;
+   constant BRAM_WOTS_KEY       : integer := BRAM_PK + 1;
+   constant BRAM_XMSS_SIG_WOTS  : integer := BRAM_WOTS_KEY + wots_len;
+   constant BRAM_XMSS_SIG_AUTH  : integer := BRAM_XMSS_SIG_WOTS + wots_len;
+   constant BRAM_XMSS_SIG       : integer := BRAM_XMSS_SIG_AUTH + tree_height;
+   constant BRAM_MESSAGE        : integer := BRAM_XMSS_SIG + 4;
+   
    constant BRAM_ADDR_SIZE : integer := 11;
    constant MAX_MLEN : integer := 2048;
-      
+
    type hash_id is record
         block_ctr : unsigned(2 downto 0);
         ctr : unsigned(ID_CTR_LEN-1 downto 0);
    end record;
+
+   -- Restauradas constantes necesarias para xmss_main_typedef
    constant zero_hash_id : hash_id := (others => (others => '0'));
    constant dont_care_hash_id : hash_id := (others => (others => '-'));
-   
-  type heights is array (tree_height downto 0) of integer range 0 to tree_height; 
-   
+
+   -- Restaurado tipo heights
+   type heights is array (tree_height downto 0) of integer range 0 to tree_height;
+
    type bram_interface_in is record
        EN           :  STD_LOGIC;
-	   WEN          :  STD_LOGIC;
-	   ADDR         :  STD_LOGIC_VECTOR(BRAM_ADDR_SIZE-1 DOWNTO 0);
-	   DIN          : STD_LOGIC_VECTOR(n*8-1 DOWNTO 0);
+       WEN          :  STD_LOGIC;
+       ADDR         :  STD_LOGIC_VECTOR(BRAM_ADDR_SIZE-1 DOWNTO 0);
+       DIN          : STD_LOGIC_VECTOR(n*8-1 DOWNTO 0);
     end record;
-    
-    type bram_interface_out is record
+
+   type bram_interface_out is record
 	   DOUT          :  STD_LOGIC_VECTOR(n*8-1 DOWNTO 0);
-    end record;
+   end record;
     
     type dual_port_bram_in is record
         a : bram_interface_in;
@@ -74,10 +69,9 @@ package params is
         addr => (others => '-'),
         din => (others => '-')
      );
-     
+
      constant dual_bram_zero : dual_port_bram_in :=(
         a=> bram_zero,
         b=> bram_zero);
-     
-     
+	
 end package;
